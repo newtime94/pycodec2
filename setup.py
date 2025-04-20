d08a46f43d5d269a90e20fb8bae7c0a4cf9da298
newtime94 <newtime@returntheslab.net>
2025-04-20 19:11:28 -0400

    Vendor headers, placeholder lib_win, and CI workflow

--- setup.py
+++ setup.py
@@ -1,42 +1,18 @@
-import Cython.Build
-from Cython.Build import cythonize
-from setuptools import Extension, setup
-import numpy as np
-import sys
+ import numpy as np
+ import sys
++import os
 
-# Extension API reference:
-# https://setuptools.pypa.io/en/latest/userguide/ext_modules.html.
-ext_modules = [
-    Extension(
-        "pycodec2.pycodec2",
-        [
-            "pycodec2/pycodec2.pyx",
-        ],
-        # We need to include the numpy headers, because we use the numpy API
-        # for array types. Without this line, the build will fail.
-        include_dirs=[np.get_include()],
-        # This line guarantees that we do not use the numpy API
-        # deprecated in 1.26.
-        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_26_API_VERSION")],
-        # The list of libraries to link against.
-        # Not including this line, will cause an error upon using pycodec2:
-        # > ImportError: dlopen(/Users/grzesiek/Code/pycodec2-202310/build/lib.macosx-13.3-arm64-cpython-311/pycodec2.cpython-311-darwin.so, 0x0002):
-        # > symbol not found in flat namespace '_codec2_700c_eq'
-        library_dirs=["lib_win"] if sys.platform == "win32" else None,
-        libraries=["libcodec2"] if sys.platform == "win32" else ["codec2"],
-    )
-]
-
-setup(
-    packages=["pycodec2"],
-    ext_modules=cythonize(ext_modules),
-    author="Grzegorz Milka",
-    author_email="grzegorzmilka@gmail.com",
-    url="https://github.com/gregorias/pycodec2",
-    cmdclass={"build_ext": Cython.Build.build_ext},
-    data_files=(
-        [("Lib/site-packages/pycodec2", ["lib_win/libcodec2.dll", "lib_win/libgcc_s_seh-1.dll"])]
-        if sys.platform == "win32"
-        else None
-    ),
-)
+ ext_modules = [
+     Extension(
+         "pycodec2.pycodec2",
+         ["pycodec2/pycodec2.pyx"],
+-        include_dirs=[np.get_include()],
++        include_dirs=[
++            np.get_include(),
++            os.path.join(os.path.dirname(__file__), "include"),
++        ],
+         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_26_API_VERSION")],
+         library_dirs=["lib_win"] if sys.platform == "win32" else None,
+         libraries=["libcodec2"] if sys.platform == "win32" else ["codec2"],
+     )
+ ]
